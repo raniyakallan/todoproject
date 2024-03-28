4d39a0cfa6bf48b61556991080f79114f8aa0be2
raniyakallan <raniyak786@gmail.com>
2024-03-28 11:48:19 +0530

    final file

---
+++ todoapp/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.add,name='add'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+]
